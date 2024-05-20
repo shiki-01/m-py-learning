a59b7048b0d04d60a0e8e899b7d87d43e28d7ca4
shiki-01 <0110shiki@gmail.com>
2024-05-20 23:40:21 +0900

    基本の見た目とルーティング

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,67 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import { page } from '$app/stores';
+	import {
+		Breadcrumb,
+		BreadcrumbItem,
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		DarkMode
+	} from 'flowbite-svelte';
+
+	let breadcrumbs: any[] = [];
+
+	$: {
+		const { pathname } = $page.url;
+		const segments = pathname.split('/').filter(Boolean);
+		breadcrumbs = segments.map((segment: any, index: number) => {
+			const href = '/' + segments.slice(0, index + 1).join('/');
+			const capitalizedSegment = segment.charAt(0).toUpperCase() + segment.slice(1);
+			return { segment: capitalizedSegment, href };
+		});
+	}
 </script>
 
-<slot></slot>
+<header>
+	<Navbar let:toggle color="primary">
+		<NavBrand href="/">
+			<img src="https://placehold.jp/150x150.png" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-red-500"
+				>M Py Learning</span
+			>
+		</NavBrand>
+		<NavHamburger on:click={toggle} />
+		<NavUl>
+			<DarkMode />
+			<NavUl>
+				<NavLi href="/" active={true}>Home</NavLi>
+				<NavLi href="/about">About</NavLi>
+				<NavLi href="/word">Word</NavLi>
+				<NavLi href="/quest">Quest</NavLi>
+			</NavUl>
+		</NavUl>
+	</Navbar>
+</header>
+<div class="bread dark:bg-gray-700">
+	<Breadcrumb aria-label="Default breadcrumb example">
+		<BreadcrumbItem href="/" home>Home</BreadcrumbItem>
+		{#each breadcrumbs as { segment, href }}
+			<BreadcrumbItem {href}>{segment}</BreadcrumbItem>
+		{/each}
+	</Breadcrumb>
+</div>
+<main class="p-8 dark:bg-gray-600">
+    <slot />
+</main>
 
-<style></style>
+<style lang="scss">
+	.bread {
+		padding: 1rem;
+	}
+	main {
+		min-height: 100vh;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,47 @@
 <script>
-	import { Alert } from 'flowbite-svelte';
+	import { Card, Button } from 'flowbite-svelte';
+	import { ArrowRightOutline } from 'flowbite-svelte-icons';
 </script>
 
-<div class="p-8">
-	<Alert>
-		<span class="font-medium">Info alert!</span>
-		Change a few things up and try submitting again.
-	</Alert>
+<div class="card">
+	<Card>
+		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+			単語・文法を覚える
+		</h5>
+		<p class="mb-3 font-normal leading-tight text-gray-700 dark:text-gray-400">
+			単語帳を使って、Python の基本的な単語や文法などを覚えよう！
+		</p>
+		<Button class="w-fit" on:click={() => location.href='/word'}>
+			Get start ! <ArrowRightOutline class="ms-2 h-6 w-6 text-white" />
+		</Button>
+	</Card>
+    <Card>
+		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+			問題を解く
+		</h5>
+		<p class="mb-3 font-normal leading-tight text-gray-700 dark:text-gray-400">
+			実践問題を解くことでさらに理解を深めよう！
+		</p>
+		<Button class="w-fit" on:click={() => location.href='/quest'}>
+			Get start ! <ArrowRightOutline class="ms-2 h-6 w-6 text-white" />
+		</Button>
+	</Card>
 </div>
+<div class="p-8">
+    <p class="text-2xl font-bold text-gray-900 dark:text-white">M Py Learning とは？</p>
+    <div class="p-2">
+        <p class="text-gray-700 dark:text-gray-400 pt-2">M Py Learning は、Python の基本的な単語や文法を学ぶための学習サイトです。</p>
+        <p class="text-gray-700 dark:text-gray-400 pt-2">
+            Python は、初心者にもわかりやすいプログラミング言語として人気があります。
+            このサイトでは、Python の基本的な単語や文法を学ぶことができます。
+        </p>
+    </div>
+</div>
+
+<style lang="scss">
+    .card {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        gap: 1rem;
+    }
+</style>
