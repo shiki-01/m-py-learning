445b1daff46db4e86642e723b32f7072ce4a1ec4
shiki-01 <0110shiki@gmail.com>
2024-05-20 22:25:27 +0900

    📁 save

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,3 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -6,16 +5,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: vitePreprocess(),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	},
-
-	extensions: ['.svelte', '.svx']
+	}
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import routify from '@roxi/routify/vite-plugin';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), routify({})]
+	plugins: [sveltekit()]
 });
